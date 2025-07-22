6ce2fffd0a698fe3a5498ecd2e6625eba42fa06e
ajaykm000563 <ajaykm000563@gmail.com>
2025-07-22 11:03:24 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import express from "express"
+import cors from "cors"
+import connectDB from './configs/MongoDB.js';
+
+
+const port = process.env.PORT || 4000
+const app = express();
+await connectDB()
+
+//middleware
+
+app.use(express.json())
+app.use(cors())
+
+
+
+//API
+app.get("/", (req, res, next) => {
+    res.send("API working")
+})
+
+app.listen(port, () => {
+    console.log(`server is runnig at port number ${port}`);
+})
